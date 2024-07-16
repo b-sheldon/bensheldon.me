13c4d4146a6290112e8e9dcbec5a04a8a800b092
Ben Sheldon <bsheldon02@gmail.com>
2024-07-15 19:57:35 -0500

    3 items on screen in carousel

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState } from 'react';
 import { useSpringCarousel } from 'react-spring-carousel';
 import { PiArrowCircleLeftThin, PiArrowCircleRightThin } from 'react-icons/pi';
-
 import Card from './card';
 
 const Projects = (props) => {
   const projects = [
     {
+      id: 'project0',
       title: 'Project 1',
       description: 'This is a description of project 1',
       skills: ['skill1', 'skill2'],
@@ -15,6 +15,7 @@ const Projects = (props) => {
       images: ['img1', 'img2'],
     },
     {
+      id: 'project1',
       title: 'Project 2',
       description: 'This is a description of project 2',
       skills: ['skill1', 'skill2'],
@@ -22,6 +23,7 @@ const Projects = (props) => {
       images: ['img1', 'img2'],
     },
     {
+      id: 'project2',
       title: 'Project 3',
       description: 'This is a description of project 3',
       skills: ['skill1', 'skill2'],
@@ -30,14 +32,19 @@ const Projects = (props) => {
     },
   ];
 
-  const { carouselFragment, slideToNextItem, slideToPrevItem } = useSpringCarousel({
+  const [currentSlide, setCurrentSlide] = useState(projects[0].id);
+
+  const { carouselFragment, slideToNextItem, slideToPrevItem, useListenToCustomEvent } = useSpringCarousel({
     withLoop: true,
+    itemsPerSlide: 3,
+    initialStartingPosition: 'center',
+    gutter: 32,
     items: projects.map((project) => {
       return {
         ...project,
         renderItem: (
           <div
-            className="relative flex flex-col items-center justify-center w-full"
+            className={`relative flex flex-col items-center justify-center w-full transition-all duration-500 ${currentSlide === project.id ? 'scale-125' : 'scale-100 opacity-50'}`}
           >
             <Card project={project} />
           </div>
@@ -46,9 +53,15 @@ const Projects = (props) => {
     }),
   });
 
+  useListenToCustomEvent((event) => {
+    if (event.eventName === 'onSlideStartChange') {
+      setCurrentSlide(event?.nextItem?.id);
+    }
+  });
+
   return (
     <div className="h-[calc(100vh-44px)] flex flex-col justify-center gap-16">
-      <div className="flex flex-row items-center justify-center w-1/2 mx-auto h-3/4 overflow-clip rounded-xl">
+      <div className="flex flex-row items-center justify-center w-screen h-3/4 overflow-clip rounded-xl">
         {carouselFragment}
       </div>
       <div className="flex flex-row self-center gap-4 text-6xl">
