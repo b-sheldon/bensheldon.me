5b669ae7d08d40ca7ca72ddf1aedccdb3677e72d
Ben Sheldon <bsheldon02@gmail.com>
2024-08-01 18:43:46 -0500

    Fix image height resizing on about me page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable jsx-a11y/mouse-events-have-key-events */
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Skill from './skill';
 import Portrait1 from '../img/portrait_1.png';
 import Portrait2 from '../img/portrait_2.png';
@@ -8,21 +8,29 @@ import Portrait2 from '../img/portrait_2.png';
 function About(props) {
   const skills = ['React', 'TailwindCSS', 'JavaScript', 'Java', 'Python', 'C++', 'C#', 'MongoDB', 'Node.js', 'Unity', 'Git', 'HTML', 'CSS', 'Firebase'];
   const [activeSkill, setActiveSkill] = useState('_____');
-  // my-[calc(60px+10vw)] <-- goes on the div that wraps the text
+  const [portraitSize, setPortraitSize] = useState(Math.min(window.innerWidth * 0.3, window.innerHeight / 2 - 60));
+
+  useEffect(() => {
+    const handleResize = () => {
+      setPortraitSize(Math.min(window.innerWidth * 0.3, window.innerHeight / 2 - 60));
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <div id="about" className="h-screen">
-      <div className="flex flex-row h-full gap-12 jusify-around mx-[10vw] pt-[60px]">
-        <div className="h-full grow basis-1 bg-blue">
-          <div className="flex flex-col justify-around h-full gap-4 overflow-hidden">
-            <div className="bg-blue-light max-h-1/2 grow basis-1 aspect-square">
-              <img src={Portrait1} alt="Portrait 1" className="object-cover rounded-full" />
-            </div>
-            <div className="bg-blue-light max-h-1/2 grow basis-1 aspect-square">
-              <img src={Portrait2} alt="Portrait 2" className="object-cover rounded-full" />
-            </div>
+      <div className="flex flex-row h-full gap-12 jusify-center mx-[10vw] pt-[60px]">
+        <div className="flex flex-col h-full w-[30vw] bg-blue items-center justify-around">
+          <div style={{ width: portraitSize, height: portraitSize }}>
+            <img src={Portrait1} alt="Portrait 1" className="object-cover rounded-full" />
+          </div>
+          <div style={{ width: portraitSize, height: portraitSize }}>
+            <img src={Portrait2} alt="Portrait 2" className="object-cover rounded-full" />
           </div>
         </div>
-        <div className="flex flex-col justify-around h-full grow basis-1">
+        <div className="flex flex-col justify-center h-full gap-24 grow basis-1">
           <div className="text-2xl">
             {'Hi, I\'m Ben. I\'m a software developer with a passion for finding creative solutions to complex problems.'}
           </div>
